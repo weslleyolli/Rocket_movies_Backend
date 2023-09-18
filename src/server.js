12820eaadba4836497c6d7dc69c11b5b2c4257c6
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-18 16:56:11 -0300

    connection with database

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors')
+const database = require('./database/sqlite')
 
 const AppError = require('./utils/AppError')
 
@@ -10,6 +11,7 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+database()
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
