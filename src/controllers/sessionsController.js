83b822f270d752482aa04061f32156edb0d0d4d6
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-10-06 10:47:21 -0300

    validating user

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,8 +1,17 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+
 class sessionsController {
     async create(request, response) {
         const { email, password } = request.body
 
-        return response.json({ email, password })
+        const user = await knex("users").where({ email }).first()
+
+        if (!user) {
+            throw new AppError("Email and password incorrect", 401)
+        }
+
+        return response.json(user)
     }
 }
 
