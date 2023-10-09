8e447442b16b62a765f37cece44924c20a509cbd
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-10-09 16:15:50 -0300

    view files

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 require('express-async-errors')
 const database = require('./database/sqlite')
-
 const AppError = require('./utils/AppError')
+const uploadConfig = require("./configs/upload")
 
 const express = require('express')
-
 const routes = require('./routes')
 
 const app = express()
 app.use(express.json())
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes)
 database()
